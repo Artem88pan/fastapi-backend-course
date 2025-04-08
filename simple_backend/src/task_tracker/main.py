b5ee0b9b38f8361b71f8e5219965870a6c804319
Панов Артём <vonap88@mail.ru>
2025-04-08 13:10:46 +0300

    настроил интеграцию с Cloudflire, но помоему что то не работат

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,37 +1,49 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 from fastapi import FastAPI, HTTPException
 from typing import List
-from simple_backend.src.task_tracker.models import TaskStorage, Task
+from task_tracker.models import Task
+from task_tracker.llm_client import CloudflareLLMClient
+from task_tracker.task_storage import RemoteTaskStorage
 
 app = FastAPI()
-storage = TaskStorage("tasks.json")
+storage = RemoteTaskStorage(bin_id='67f39cb88a456b796683ff72', api_key='$2a$10$R6hU9ZDFC2IgNH8K7IPQxexqGRz7.v1vo8A7nJcAq7MIoXbQstHXu')
+llm = CloudflareLLMClient(
+    "https://task-helper-ai.panov89119018073.workers.dev"
+)
 
 
 
 
 @app.get("/tasks", response_model=List[Task])
-def get_tasks():
-    return storage.get_all
+async def get_tasks():
+    return await storage.get_all
 
 @app.post("/tasks", response_model=Task)
-def create_task(task: Task):
+async def create_task(task: Task):
     try:
-        storage.add(task)
+        solution = await llm.get_solution(task.name)
+        task.name += f"\n\n Решение от ИИ: \n{solution}"
+        await storage.add(task)
         return task
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 @app.put("/tasks/{task_id}", response_model=Task)
-def update_task(task_id: int, updated_task: Task):
-    if storage.update(task_id, updated_task):
-        return updated_task
+async def update_task(task_id: int, updated: Task):
+    result = await storage.update(task_id, updated)
+    if result:
+        return result
     raise HTTPException(status_code=404, detail="задача не найдена")    
 
 @app.delete("/tasks/{task_id}")
-def delete_task(task_id: int):
-    deleted = storage.delete(task_id)
-    if deleted:
-        return deleted
+async def delete_task(task_id: int):
+    result = await storage.delete(task_id)
+    if result:
+        return result
     raise HTTPException(status_code=404, detail="задача не найдена")    
-
 
 
--- simple_backend/src/task_tracker/task_storage.py
+++ simple_backend/src/task_tracker/task_storage.py
@@ -1,51 +1,57 @@
 from typing import List, Optional
-from models import Task
+from task_tracker.models import Task
+import httpx
 
 
 
-class TaskStorage:
-    def __init__(self, filename: str= 'task.json'):
-        self.filename = filename
-        self._ensure_file()
+class RemoteTaskStorage:
+    def __init__(self, bin_id: str, api_key: str):
+        self.base_url = f"https://api.jsonbin.io/v3.b.{bin_id}"
+        self.headers = {
+            "X-Master-Key": api_key,
+            "Content-Type": "application/json"
+        }
 
-    def _ensure_file(self):
-        if not os.path.exists(self.filename):
-            self._save([])
-
-    def _load(self) -> List[Task]:
-        with open(self.filename, "r", encoding="utf-8") as f:
-            data = json.load(f)
-            return [Task(**item) for item in data]
-                
     
-    def _save(self, tasks: List[Task]):
-        with open(self.filename, "w", encoding="utf-8") as f:
-            json.dump([task.dict() for task in tasks], f, ensure_ascii=False, indent=4)
-
-    def get_all(self) -> List[Task]:
-        return self._load
     
-    def add(self, task: Task):
-        tasks = self._load()
+    async def save_all(self, tasks: List[Task]):
+        payload = [task.model_dump() for task in tasks]
+        async with httpx.AsyncClient() as client:
+            response = await client.put(self.base_url, headers=self.headers, json=payload)
+            response.raise_for_status()
+
+
+
+    async def get_all(self) -> List[Task]:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(self.base_url, headers=self.headers)
+            response.raise_for_status()
+            data = response.json()["record"]
+            return[Task(**item) for item in data]
+        
+    
+    async def add(self, task: Task):
+        tasks = await self.get_all()
         if any(t.id == task.id for t in tasks):
-              raise ValueError('задача с таким индификатором уже существует')
+            raise ValueError('задача с таким индификатором уже существует')
         tasks.append(task)
-        self._save(tasks)
-    def update(self, task_id: int, updated: Task) -> bool:
-        tasks = self._load()
+        await self.get_all(tasks)
+
+    async def update(self, task_id: int, updated: Task) -> Optional[Task]:
+        tasks = await self.get_all()
         for i, t in enumerate(tasks):
             if t.id == task_id:
                 tasks[i] = updated
-                self._save(tasks)
-                return True
-        return False
+                await self.save_all(tasks)
+                return updated
+        return None
 
-    def delete(self, task_id: int) -> Optional[Task]:
-        tasks = self._load()
+    async def delete(self, task_id: int) -> Optional[Task]:
+        tasks = await self.get_all
         for t in tasks:
             if t.id == task_id:
                 tasks.remove(t)
-                self._save(tasks)
+                await self.save_all(tasks)
                 return t
         return None
                         
